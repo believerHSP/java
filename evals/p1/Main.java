04a5eb87c85f404fd6a2c01f8be7ee592bba5729
believerHSP <pokhariyahimansu@gmail.com>
2022-05-23 15:07:19 +0530

    String

--- evals/p1/Main.java
+++ evals/p1/Main.java
@@ -1,5 +1,5 @@
-package evals.p1;
+String is a predefined java class belongs to java.lang package.
+thihs string class is used to create String objects in JAVA.
+ how to create:- String str1="welcome";
 
-public class Main {
-
-}
+here the str1 will points to the address of the String object created in the string pool of RAM.
